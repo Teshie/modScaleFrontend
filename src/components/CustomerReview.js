da22c1c734ba0f75adea4c69bde4c057261248e1
Teshie <teshomeaychiluhem@gmail.com>
2023-03-12 03:53:58 -0700

    animation fix

--- src/components/CustomerReview.js
+++ src/components/CustomerReview.js
@@ -10,29 +10,42 @@ import { customerReview } from "../data/data";
 import { revenueManagement } from "../data/data";
 import ReactPaginate from "react-paginate";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-
+import { motion, AnimatePresence } from "framer-motion";
 const CustomerReview = () => {
-  const reviewsGridRef = useRef(null);
-
-  useEffect(() => {
-    if (reviewsGridRef.current) {
-      const reviewsGridHeight = reviewsGridRef.current.offsetHeight;
-      const parentContainer = reviewsGridRef.current.parentNode;
-      parentContainer.style.height = `${reviewsGridHeight}px`;
-    }
-  }, []);
-
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 4;
   const pageCount = Math.ceil(customerReview.length / itemsPerPage);
 
-  const handlePageClick = ({ selected }) => {
-    setCurrentPage(selected);
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage);
   };
 
-  const offset = currentPage * itemsPerPage;
-  const currentReviews = customerReview.slice(offset, offset + itemsPerPage);
-  const fadeTransitionDuration = 300;
+  const currentReviews = customerReview.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  );
+
+  const duration = 0.4;
+
+  const defaultStyle = {
+    transition: `opacity ${duration}s ease-in-out`,
+    opacity: 0,
+  };
+
+  const transitionStyles = {
+    entering: { opacity: 0 },
+    entered: { opacity: 1 },
+  };
+
+  const paginateVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
+  };
+
+  const cardVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
 
   return (
     <div className="width mx-auto mt-10">
@@ -46,52 +59,43 @@ const CustomerReview = () => {
           backgroundSize: "cover",
         }}
       >
-        <div className="flex justify-center items-center h-full overflow-hidden mx-auto">
-          <div ref={reviewsGridRef} className="flex flex-col space-y-6">
-            <TransitionGroup>
-              <CSSTransition
-                key={currentPage}
-                timeout={fadeTransitionDuration}
-                classNames="fade"
-              >
-                <div className="grid  place-items-center xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10 ">
-                  {currentReviews.map((items) => (
-                    <CSSTransition
-                      key={items.id}
-                      timeout={fadeTransitionDuration}
-                      classNames="fade-review"
-                    >
-                      <div className="h-76 cursor-pointer hover:shadow-lg bg-white rounded-lg  w-56   flex flex-col justify-start p-3.5">
-                        <div className="flex justify-start items-start ">
-                          <img
-                            className="h-8 w-8"
-                            src={comment}
-                            alt="commentIcon"
-                          />
-                        </div>
-                        <div className="mt-4 text-gray-800 ">
-                          <p className="text-base text-left">{items.comment}</p>
-                        </div>
+        <div className="flex justify-center items-center mx-auto">
+          <div className="flex flex-col space-y-6">
+            <motion.div
+              className="grid place-items-center xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10"
+              style={{ height: "auto", overflow: "hidden" }}
+              variants={paginateVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {currentReviews.map((item) => (
+                <motion.div
+                  key={item.id}
+                  variants={cardVariants}
+                  transition={{ duration }}
+                  className="h-76 cursor-pointer hover:shadow-lg bg-white rounded-lg w-56 flex flex-col justify-start p-3.5"
+                >
+                  <div className="flex justify-start items-start">
+                    <img className="h-8 w-8" src={comment} alt="commentIcon" />
+                  </div>
+                  <div className="mt-4 text-gray-800 ">
+                    <p className="text-base text-left">{item.comment}</p>
+                  </div>
 
-                        <div className="flex items-center mt-2">
-                          <img
-                            className="h-8 w-8 rounded-full mr-2"
-                            src="https://i.pravatar.cc/50"
-                            alt="User Avatar"
-                          />
-                          <div>
-                            <p className=" ">{items.username}</p>
-                            <p className="text-gray-500 text-sm">
-                              {items.company}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </CSSTransition>
-                  ))}
-                </div>
-              </CSSTransition>
-            </TransitionGroup>
+                  <div className="flex items-center mt-2">
+                    <img
+                      className="h-8 w-8 rounded-full mr-2"
+                      src="https://i.pravatar.cc/50"
+                      alt="User Avatar"
+                    />
+                    <div>
+                      <p className=" ">{item.username}</p>
+                      <p className="text-gray-500 text-sm">{item.company}</p>
+                    </div>
+                  </div>
+                </motion.div>
+              ))}
+            </motion.div>
 
             <div className="flex justify-between items-center mt-6  ">
               <ReactPaginate
@@ -110,7 +114,6 @@ const CustomerReview = () => {
                 style={{ "--pagination-page-display": "none" }}
               />
             </div>
-            {/* <img src={map} /> */}
           </div>
         </div>
       </div>
